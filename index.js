22c423808f7dcf502e20de2b5f9be8ccc7c67906
Satyam Kumar <satyamkumarem@gmail.com>
2025-12-23 09:32:44 +0530

    Update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const session = require("express-session");
 const path = require("path");
 
 const app = express();
@@ -8,12 +9,20 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("views"));
 
-// Home page
+app.use(
+  session({
+    secret: "live-session-secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Home
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-// Form submit
+// Login
 app.post("/login", (req, res) => {
   const { password } = req.body;
 
@@ -21,12 +30,25 @@ app.post("/login", (req, res) => {
     return res.send("<h2>❌ Wrong Password</h2><a href='/'>Go Back</a>");
   }
 
+  // Mark user as authenticated
+  req.session.isAuthenticated = true;
   res.redirect("/pay");
 });
 
-// Payment page
+// Protect Pay Route
 app.get("/pay", (req, res) => {
+  if (!req.session.isAuthenticated) {
+    return res.redirect("/");
+  }
+
   res.sendFile(path.join(__dirname, "views", "pay.html"));
+});
+
+// Logout (optional)
+app.get("/logout", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
 });
 
 app.listen(PORT, () => {
